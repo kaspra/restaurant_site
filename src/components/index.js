c6f2fc935c3192dbca5bc8cb6334087a2e4c44dc
kaspra <92664226+kaspra@users.noreply.github.com>
2024-01-01 19:04:17 +0530

    Launch

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as FooterOverlay } from './Footer/FooterOverlay';
+export { default as Newsletter } from './Footer/Newsletter';
+export { default as MenuItem } from './Menuitem/MenuItem';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as SubHeading } from './SubHeading/SubHeading';
